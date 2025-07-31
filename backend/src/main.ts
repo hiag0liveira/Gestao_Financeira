f40e4523e0979f99ddd9431a5ffd1a9876690ff9
Hiago Mendes <hiagophd@gmail.com>
2025-07-31 09:26:01 -0300

    feat(cors): habilitar CORS para permitir requisições do frontend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,6 +6,14 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+
+  app.enableCors({
+    origin: 'http://localhost:3001',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+
   app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
